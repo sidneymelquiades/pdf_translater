75175619baa37c597fcd3471a89dd9a5f1204b5e
sidneymelquiadesdantas <sidney.dantas@hdi.com.br>
2023-10-26 17:14:27 -0300

    add pdf 2 image function

--- scripts/external_libraries.py
+++ scripts/external_libraries.py
@@ -1,2 +1,5 @@
 from pdf2image import convert_from_path
 import os
+
+from PIL import Image
+from pytesseract import pytesseract
--- scripts/functions.py
+++ scripts/functions.py
@@ -11,6 +11,26 @@ def pdf_2_image(pdf_path: str):
         assets_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'file_output_image\page'))
         images[i].save(assets_dir + str(i) + '.jpg', 'JPEG')
 
+def image_2_text(path_to_tesseract : str, image_path : str ):
+
+
+    # Opening the image & storing it in an image object
+    img = Image.open(image_path)
+
+    # Providing the tesseract executable
+    # location to pytesseract library
+    pytesseract.tesseract_cmd = path_to_tesseract
+
+    # Passing the image object to image_to_string() function
+    # This function will extract the text from the image
+    text = pytesseract.image_to_string(img)
+
+    # Displaying the extracted text
+    return text[:-1]
+
 if __name__=='__main__':
 
-
+    path_to_tesseract = r"C:\Users\sidneyd\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+    assets_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'file_output_image\page'))
+    image_path = assets_dir + "11.jpg"
+    print(image_2_text(path_to_tesseract, image_path))
